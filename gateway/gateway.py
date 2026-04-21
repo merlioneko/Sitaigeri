f971bdafe02f5cd1cc23ba855a36f43b7c6243be
Morikawa <morikawa.reo@yamato-sewing.co.jp>
2026-04-21 16:27:23 +0900

    ざっくりドメインっぽい感じの駆動

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -5,7 +5,7 @@ from typing import Protocol
 
 class ILlmPort(Protocol):
     @abstractmethod
-    def response(self, system: (SystemPrompt| str), user: (UserPrompt|str)) -> str:
+    def response(self, system: (SystemPrompt| str), user: (UserPrompt|str), output_format: (dict|None) = None) -> str:
         pass
 
 class LmStudioGateway(ILlmPort):
@@ -26,20 +26,21 @@ class LmStudioGateway(ILlmPort):
         if output_format is not None:
             response = self.client.chat.completions.create(
                 model=self.model_name,
-                messages=[
-                    system.to_json(),
-                    user.to_json()
-                ],
+                messages=[system.to_json(),user.to_json()],
                 response_format=output_format
             )
         else:
             response = self.client.chat.completions.create(
                 model=self.model_name,
-                messages=[
-                    system.to_json(),
-                    user.to_json()
-                ]
+                messages=[system.to_json(),user.to_json()]
             )
         
         content = response.choices[0].message.content
         return content
+
+class TestGateway(ILlmPort):
+    def __init__(self, url: str, model_name: str):
+        self.url = url
+        self.model_name = model_name
+    def response(self, system: (SystemPrompt| str), user: (UserPrompt|str), output_format: (dict|None) = None) -> str:
+        return "これはテスト用のダミー応答です。"
